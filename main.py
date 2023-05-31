50241da9bc2fba9045dc1be61d94cfd432f62faf
jin3137 <jin3137@gmail.com>
2023-05-31 12:26:38 +0900

    불필요한 파일 제거
    [이성국]
    
    1. 불필요한 파일 제거

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 from flab_bada.controller.controller import controller_router
 from flab_bada.domain.users.user_controller import user_router
-from flab_bada.redis_client.application import redis_router
 
 
 app = FastAPI()
 app.include_router(controller_router)
 app.include_router(user_router)
-app.include_router(redis_router)
